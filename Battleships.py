e3913761c363783ae9873a00476098e2b09adf7a
muff1n0 <tchen51@stuy.edu>
2023-03-15 22:27:37 -0400

    add Ship.checkRotate()

--- Battleships.py
+++ Battleships.py
@@ -28,11 +28,11 @@ class Ship:
     alive = True
 
 
-    def __init__(self, length, orientation, board): #up, down, left, right
+    def __init__(self, length, orientation, board, location = ""): #up, down, left, right
         self.length = length
         self.orientation = orientation
         self.board = board
-
+        self.location = location
 
     @staticmethod
     def locationSwitch(location):
@@ -70,6 +70,27 @@ class Ship:
         return False in [patch.shipHere for patch in ship_span]
 
 
+    def removeShip(self):
+        """
+        Sets all the patches that the ship occupies to not be occupied anymore 
+        None -> None
+        """
+        if self.location == "":
+            return 
+        row_index, column_index = Ship.locationSwitch(self.location)
+        self.board.board[row_index][column_index].headShipHere = False
+        if self.orientation == "up": 
+            ship_span_indexes = [(row_index - row, column_index) for row in range(self.length)]
+        elif self.orientation == "down":
+            ship_span_indexes = [(row_index + row, column_index) for row in range(self.length)]
+        elif self.orientation == "left":
+            ship_span_indexes = [(row_index, column_index - column) for column in range(self.length)]
+        elif self.orientation == "right":
+            ship_span_indexes = [(row_index, column_index + column) for column in range(self.length)]
+        for row, column in ship_span_indexes:
+            self.board.board[row][column].shipHere = False
+
+
     def moveShip(self, location): #runs after computer checks if the location is available
         """
         After a location is validated for a ship, this function sets the shipHere for all the 
@@ -77,6 +98,7 @@ class Ship:
         to be True
         String -> None
         """
+        self.location = location
         row_index, column_index = Ship.locationSwitch(location)
         self.board.board[row_index][column_index].headShipHere = True 
         if self.orientation == "up": 
@@ -89,6 +111,33 @@ class Ship:
             ship_span_indexes = [(row_index, column_index + column) for column in range(self.length)]
         for row, column in ship_span_indexes:
             self.board.board[row][column].shipHere = True
+
+
+    def checkRotate(self, orientation):
+        """
+        Checks if the hypothetical patches a ship will occupy if it is rotated is valid \n
+        String -> Boolean
+        """
+        if orientation == self.orientation:
+            return True
+        row_index, column_index = Ship.locationSwitch(self.location)
+        ship_span = []
+        if orientation == "up": 
+            ship_span_indexes = [(row_index - row, column_index) for row in range(self.length)]
+        elif orientation == "down":
+            ship_span_indexes = [(row_index + row, column_index) for row in range(self.length)]
+        elif orientation == "left":
+            ship_span_indexes = [(row_index, column_index - column) for column in range(self.length)]
+        elif orientation == "right":
+            ship_span_indexes = [(row_index, column_index + column) for column in range(self.length)]
+        for row, column in ship_span_indexes:
+            if row < 0 or column < 0:
+                return False
+            try:
+                ship_span.append(self.board.board[row][column])
+            except IndexError:
+                return False
+        return False in [patch.shipHere for patch in ship_span]
 
 
 class Game:
